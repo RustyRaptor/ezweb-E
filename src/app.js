07fe2c706bd411af9dbe2c932c88201fb4df407c
RustyRaptor <ziadarafat1@gmail.com>
2018-07-15 10:17:00 -0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PhotoGallery from './components/PhotoGallery';
+import 'normalize.css/normalize.css';
+// import './styles/styles.scss';
+import './styles/components/bootstrap.css';
+import './styles/bootswatch.scss';
+
+
+ReactDOM.render(<PhotoGallery />, document.getElementById('app'));
---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
+
+
+const photos = [
+  { src: '/images/people/0001.jpg', width: 4, height: 3 },
+  { src: '/images/people/0002.jpg', width: 1, height: 1 },
+  { src: '/images/people/0003.jpg', width: 3, height: 4 },
+  { src: '/images/people/0004.jpg', width: 3, height: 4 },
+  { src: '/images/people/0005.jpg', width: 3, height: 4 },
+  { src: '/images/people/0006.jpg', width: 4, height: 3 },
+  { src: '/images/people/0007.jpg', width: 3, height: 4 },
+  { src: '/images/people/0008.jpg', width: 4, height: 3 },
+  { src: '/images/people/0009.jpg', width: 4, height: 3 }
+];
+
+
+export default class PhotoGallery extends React.Component {
+  constructor() {
+    super();
+    this.state = { currentImage: 0 }; 
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+  }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Gallery photos={photos} onClick={this.openLightbox} />
+        <Lightbox images={photos}
+          onClose={this.closeLightbox}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        />
+      </div>
+    )
+  }
+}
